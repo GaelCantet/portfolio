0aa10197a21d677d21ace5fba93868064057b891
Gael Cantet <gael.cantet@orange.fr>
2021-04-12 17:15:45 +0200

    simplification js du bouton menu

--- js/script.js
+++ js/script.js
@@ -5,11 +5,5 @@ toggleMenu.addEventListener('click', function() {
   const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
   toggleMenu.setAttribute('aria-expanded', !open);
   menu.hidden = !menu.hidden;
-  if(toggleMenu.classList.contains('menu-closed')){
-    toggleMenu.classList.remove('menu-closed');
-    toggleMenu.classList.add('menu-open');
-  } else {
-    toggleMenu.classList.remove('menu-open');
-    toggleMenu.classList.add('menu-closed');
-  }
+  toggleMenu.classList.toggle('menu-open');
 });
